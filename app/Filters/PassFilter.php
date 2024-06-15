c00826ab353428f8eab5fd47f85876203c410ced
asmi046 <asmi046@gmail.com>
2024-06-15 13:54:38 +0300

    Or wher

--- app/Filters/PassFilter.php
+++ app/Filters/PassFilter.php
@@ -7,8 +7,7 @@ class PassFilter extends QueryFilter {
     public function serch($serch) {
         if (!empty($serch)) {
             $mserch = "%".$serch."%";
-            $this->builder->where("truc_number", "LIKE", $mserch);
-            $this->builder->orWhere("email",  "LIKE", $mserch);
+            $this->builder->where("truc_number", "LIKE", $mserch)->orWhere("email",  "LIKE", $mserch);
         }
 
     }
@@ -20,17 +19,6 @@ class PassFilter extends QueryFilter {
                 $query->where('sys_status', $sys_statuse);
             });
 
-            // if (($sys_statuse === 'Закончился')||($sys_statuse === 'Анулирован')) {
-            //     $this->builder->whereHas('no_active_numbers', function ($query) use ($sys_statuse) {
-            //         $query->where('sys_status', $sys_statuse);
-            //     });
-            // } else {
-            //     $this->builder->whereHas('active_numbers', function ($query) use ($sys_statuse) {
-            //         $query->where('sys_status', $sys_statuse);
-            //     });
-
-            // }
-
         }
 
     }
@@ -41,16 +29,6 @@ class PassFilter extends QueryFilter {
             $this->builder->whereHas('last_pass', function ($query) use ($series) {
                 $query->where('series', $series);
             });
-
-            // if ((request('sys_statuse') === 'Закончился')||(request('sys_statuse') === 'Анулирован')) {
-            //     $this->builder->whereHas('no_active_numbers', function ($query) use ($series) {
-            //         $query->where('series', $series);
-            //     });
-            // } else {
-            //     $this->builder->whereHas('active_numbers', function ($query) use ($series) {
-            //         $query->where('series', $series);
-            //     });
-            // }
         }
 
     }
