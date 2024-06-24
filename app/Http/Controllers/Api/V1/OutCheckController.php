0d3a2e33d1b3b694a5f7e32462f673b5ec291dcb
asmi046 <asmi046@gmail.com>
2024-06-24 15:54:37 +0300

    Новый

--- app/Http/Controllers/Api/V1/OutCheckController.php
+++ app/Http/Controllers/Api/V1/OutCheckController.php
@@ -8,6 +8,42 @@ use App\Services\ChecNumberServices;
 
 class OutCheckController extends Controller
 {
+
+    public function get_all_number_variant($number, &$all_var) {
+        $i=0;
+        do {
+            if ((ctype_digit($number[$i]) && ctype_alpha($number[$i+1]))||(ctype_alpha($number[$i]) && ctype_digit($number[$i+1])) ) {
+                $ns = str_replace($number[$i].$number[$i+1],$number[$i]." ".$number[$i+1],$number);
+                $all_var[] = $ns;
+                $this->get_all_number_variant($ns, $all_var);
+            }
+
+            $i++;
+        } while ($i<strlen($number)-2);
+
+        return $all_var;
+    }
+
+    public function get_number_info_for_site_zag(ChecNumberServices $detail_service, Request $request) {
+        $number = $request->input('number');
+        if (!$number) abort(403, "Недостаточно данных");
+
+        $all_var = [];
+        $all_var[] = $number;
+	    $rez_var = $this->get_all_number_variant($number, $all_var);
+
+dump($rez_var);
+
+        $passes=[];
+        foreach ($rez_var as $number_elem)
+        {
+            $info = $detail_service->chec_number($number_elem, "site");
+            $passes = array_merge($passes, $info);
+        }
+
+        return $passes;
+    }
+
     public function get_number_info_for_site(ChecNumberServices $detail_service, Request $request) {
         $number = $request->input('number');
         if (!$number) abort(403, "Недостаточно данных");
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -19,5 +19,6 @@ use App\Http\Controllers\Api\V1\PassCreateInfoController;
 Route::middleware('api')->group(function () {
     Route::get('/get_number_info_out', [OutCheckController::class, "get_number_info_out"])->name('get_number_info_out');
     Route::get('/get_number_info_for_site', [OutCheckController::class, "get_number_info_for_site"])->name('get_number_info_for_site');
+    Route::get('/get_number_info_for_site_zag', [OutCheckController::class, "get_number_info_for_site_zag"])->name('get_number_info_for_site_zag');
     Route::post('/create_new_pass', [PassCreateInfoController::class, "create_new_pass"])->name('create_new_pass');
 });
