acf5b3d2fe069d62a84717d9a35bf51d57158bc7
asmi046 <asmi046@gmail.com>
2024-07-29 18:18:36 +0300

    email_dop

--- app/Services/MassAlertServices.php
+++ app/Services/MassAlertServices.php
@@ -40,15 +40,21 @@ class MassAlertServices {
 
         $adt_tosend = config('notification_adr.adr_to_send');
         // dump($an);
-        if (config('app.env') === "production")
+        if (config('app.env') === "production") {
             $adt_tosend[] = $an->truc->email;
+            $adt_tosend[] = $an->truc->email_dop;
+        }
+
+
         sleep(rand(1,2));
+
         Mail::to($adt_tosend)->send(new AnnulMail($an->truc->truc_number, $serias.$number, $an->type_pass));
 
         return [
             'pass' => $serias.$number,
             'truc_number' => $an->truc->truc_number,
             'email' => $an->truc->email,
+            'email_dop' => $an->truc->email_dop,
             'time' => $an->type_pass,
             'result' => 1,
         ];
